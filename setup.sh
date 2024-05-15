c4100e7af9ff63901895b2e40359d216c8dfce06
Claudemir Todo Bom <claudemir@todobom.com>
2024-05-15 18:32:56 -0300

    Added setup.sh script

---
+++ setup.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Verifica se está rodando como root
+if [[ $EUID -ne 0 ]]; then
+   echo "Este script deve ser executado como root" 
+   exit 1
+fi
+
+# Função para mostrar a mensagem de uso
+show_usage() {
+    echo -e     "Uso: \n\n      $0 <backend_host> <frontend_host> <email>\n\n"
+    echo -e "Exemplo: \n\n      $0 api.ticketz.exemplo.com.br ticketz.exemplo.com.br email@exemplo.com.br\n\n"
+}
+
+# Verifica se os parâmetros estão corretos
+if [ -z "$3" ]; then
+    show_usage
+    exit 1
+fi
+
+# Atribui os valores dos parâmetros a variáveis
+backend_host="$1"
+frontend_host="$2"
+email="$3"
+
+# Passo 1: Providencia uma VPS zerada e aponta os hostnames do teu DNS para ela
+# Passo 2: Instala o docker
+curl -sSL https://get.docker.com | sh
+
+# Passo 3: Baixa o projeto e entra na pasta
+git clone https://github.com/ticketz-oss/ticketz-docker-acme.git
+cd ticketz-docker-acme
+
+# Passo 4: Configura os hostnames
+sed "/^BACKEND_HOST/s/.*/BACKEND_HOST=$backend_host/"  \
+  | sed "/^FRONTEND_HOST/s/.*/FRONTEND_HOST=$frontend_host/" \
+  | sed "/^EMAIL_ADDRESS/s/.*/EMAIL_ADDRESS=$email/" < example.env-backend > .env-backend
+
+sed "/^BACKEND_HOST/s/.*/BACKEND_HOST=$backend_host/" \
+  | sed "/^FRONTEND_HOST/s/.*/FRONTEND_HOST=$frontend_host/" \
+  | sed "/^EMAIL_ADDRESS/s/.*/EMAIL_ADDRESS=$email/" < example.env-frontend > .env-frontend
+
+# Passo 5: Sobe os containers
+docker compose up -d
+
+cat << EOF
+A geração dos certificados e a inicialização do serviço pode levar
+alguns minutos.
+
+Após isso você pode acessar o Ticketz pela URL
+
+        https://${frontend_host}
+        
+EOF
