18533d715f6b7b760c03d0a64faf622e2a408e01
Claudemir Todo Bom <claudemir@todobom.com>
2024-07-28 14:49:51 -0300

    Setup changes to support only one hostname

--- setup.sh
+++ setup.sh
@@ -36,27 +36,26 @@ if [ -n "$3" ] ; then
     email="$3"
 else 
     backend_host="$1"
-    backend_path="/backend"
+    backend_path="\\/backend"
     frontend_host="$1"
     email="$2"
 fi
 
 emailregex="^[a-z0-9!#\$%&'*+/=?^_\`{|}~-]+(\.[a-z0-9!#$%&'*+/=?^_\`{|}~-]+)*@([a-z0-9]([a-z0-9-]*[a-z0-9])?\.)+[a-z0-9]([a-z0-9-]*[a-z0-9])?\$"
-if ![[ $email =~ $regex ]] ; then
+if ! [[ $email =~ $emailregex ]] ; then
     echo "email inválido"
     show_usage
     exit 1
 fi
 
 # Passo 1: Providencia uma VPS zerada e aponta os hostnames do teu DNS para ela
-# Passo 2: Instala o docker
+# Passo 2: Instala o docker / apenas se já não tiver instalado
 which docker > /dev/null || curl -sSL https://get.docker.com | sh
 
 # Passo 3: Baixa o projeto e entra na pasta
 [ -d ticketz-docker-acme ] || git clone https://github.com/ticketz-oss/ticketz-docker-acme.git
 cd ticketz-docker-acme
 if git diff-index --quiet HEAD -- ; then
-  echo "Salvando alterações do repositório"
   git stash push &> /dev/null
   echo "Atualizando repositório"
   if ! git pull &> pull.log; then
@@ -74,18 +73,31 @@ fi
 # Passo 4: Configura os hostnames
 cat example.env-backend \
   | sed -e "s/^BACKEND_HOST=.*/BACKEND_HOST=$backend_host/g"  \
-  | sed -e "s/^BACKEND_PATH=.*/BACKEND_HOST=$backend_path/g"  \
+  | sed -e "s/^BACKEND_PATH=.*/BACKEND_PATH=$backend_path/g"  \
   | sed -e "s/^FRONTEND_HOST=.*/FRONTEND_HOST=$frontend_host/g" \
   | sed -e "s/^EMAIL_ADDRESS=.*/EMAIL_ADDRESS=$email/g"  > .env-backend
 
 cat example.env-frontend \
   | sed -e "s/^BACKEND_HOST=.*/BACKEND_HOST=$backend_host/g" \
-  | sed -e "s/^BACKEND_PATH=.*/BACKEND_HOST=$backend_path/g" \
+  | sed -e "s/^BACKEND_PATH=.*/BACKEND_PATH=$backend_path/g" \
   | sed -e "s/^FRONTEND_HOST=.*/FRONTEND_HOST=$frontend_host/g" \
   | sed -e "s/^EMAIL_ADDRESS=.*/EMAIL_ADDRESS=$email/g" > .env-frontend
 
+## inclui configuração para o acme-companion se o backend tiver host a parte
+[ -z "$backend_path" ] && cat >> .env-backend << EOF
+# Normalmente não é necessário alterar estes valores
+VIRTUAL_HOST=\${BACKEND_HOST}
+VIRTUAL_PORT=3000
+LETSENCRYPT_HOST=\${BACKEND_HOST}
+LETSENCRYPT_EMAIL=\${EMAIL_ADDRESS}
+EOF
+
 # Passo 5: Sobe os containers
-docker compose down && docker compose up -d
+if ! ( docker compose down && docker compose up -d ); then
+    echo "Falha ao reiniciar containers"
+    echo -e "\n\nAlterações precisam ser verificadas manualmente, procure suporte se necessário\n\n"
+    exit 1
+fi
 
 cat << EOF
 A geração dos certificados e a inicialização do serviço pode levar
@@ -96,3 +108,6 @@ Após isso você pode acessar o Ticketz pela URL
         https://${frontend_host}
         
 EOF
+
+echo "Removendo imagens anteriores..."
+docker system prune -af &> /dev/null
