0bf58fd490e4b81e32cb13b0d62b4335afec191b
Claudemir Todo Bom <claudemir@todobom.com>
2024-07-28 13:55:58 -0300

    setup with only one hostname

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 
 # Função para mostrar a mensagem de uso
 show_usage() {
-    echo -e     "Uso: \n\n      curl -sSL https://get.ticke.tz | sudo bash -s <backend_host> <frontend_host> <email>\n\n"
-    echo -e "Exemplo: \n\n      curl -sSL https://get.ticke.tz | sudo bash -s api.ticketz.exemplo.com.br ticketz.exemplo.com.br email@exemplo.com.br\n\n"
+    echo -e     "Uso: \n\n      curl -sSL https://get.ticke.tz | sudo bash -s <frontend_host> <email>\n\n"
+    echo -e "Exemplo: \n\n      curl -sSL https://get.ticke.tz | sudo bash -s ticketz.exemplo.com.br email@exemplo.com.br\n\n"
 }
 
 # Verifica se está rodando usando o bash
@@ -21,37 +21,71 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 # Verifica se os parâmetros estão corretos
-if [ -z "$3" ]; then
+if [ -z "$2" ]; then
     show_usage
     exit 1
 fi
 
+emailregex="^[a-z0-9!#\$%&'*+/=?^_\`{|}~-]+(\.[a-z0-9!#$%&'*+/=?^_\`{|}~-]+)*@([a-z0-9]([a-z0-9-]*[a-z0-9])?\.)+[a-z0-9]([a-z0-9-]*[a-z0-9])?\$"
+
 # Atribui os valores dos parâmetros a variáveis
-backend_host="$1"
-frontend_host="$2"
-email="$3"
+if [ -n "$3" ] ; then
+    backend_host="$1"
+    backend_path=""
+    frontend_host="$2"
+    email="$3"
+else 
+    backend_host="$1"
+    backend_path="/backend"
+    frontend_host="$1"
+    email="$2"
+fi
+
+emailregex="^[a-z0-9!#\$%&'*+/=?^_\`{|}~-]+(\.[a-z0-9!#$%&'*+/=?^_\`{|}~-]+)*@([a-z0-9]([a-z0-9-]*[a-z0-9])?\.)+[a-z0-9]([a-z0-9-]*[a-z0-9])?\$"
+if ![[ $email =~ $regex ]] ; then
+    echo "email inválido"
+    show_usage
+    exit 1
+fi
 
 # Passo 1: Providencia uma VPS zerada e aponta os hostnames do teu DNS para ela
 # Passo 2: Instala o docker
-curl -sSL https://get.docker.com | sh
+which docker > /dev/null || curl -sSL https://get.docker.com | sh
 
 # Passo 3: Baixa o projeto e entra na pasta
-git clone https://github.com/ticketz-oss/ticketz-docker-acme.git
+[ -d ticketz-docker-acme ] || git clone https://github.com/ticketz-oss/ticketz-docker-acme.git
 cd ticketz-docker-acme
+if git diff-index --quiet HEAD -- ; then
+  echo "Salvando alterações do repositório"
+  git stash push &> /dev/null
+  echo "Atualizando repositório"
+  if ! git pull &> pull.log; then
+    echo "Falha ao Atualizar repositório, verifique arquivo pull.log"
+    echo -e "\n\nAlterações precisam ser verificadas manualmente, procure suporte se necessário\n\n"
+    exit 1
+  fi
+  if ! git stash pop &> stash-pop.log; then
+    echo "Falha ao recuperar alterações salvas, verifique arquivo stash-pop.log"
+    echo -e "\n\nAlterações precisam ser verificadas manualmente, procure suporte se necessário\n\n"
+    exit 1
+  fi
+fi
 
 # Passo 4: Configura os hostnames
 cat example.env-backend \
   | sed -e "s/^BACKEND_HOST=.*/BACKEND_HOST=$backend_host/g"  \
+  | sed -e "s/^BACKEND_PATH=.*/BACKEND_HOST=$backend_path/g"  \
   | sed -e "s/^FRONTEND_HOST=.*/FRONTEND_HOST=$frontend_host/g" \
   | sed -e "s/^EMAIL_ADDRESS=.*/EMAIL_ADDRESS=$email/g"  > .env-backend
 
 cat example.env-frontend \
   | sed -e "s/^BACKEND_HOST=.*/BACKEND_HOST=$backend_host/g" \
+  | sed -e "s/^BACKEND_PATH=.*/BACKEND_HOST=$backend_path/g" \
   | sed -e "s/^FRONTEND_HOST=.*/FRONTEND_HOST=$frontend_host/g" \
   | sed -e "s/^EMAIL_ADDRESS=.*/EMAIL_ADDRESS=$email/g" > .env-frontend
 
 # Passo 5: Sobe os containers
-docker compose up -d
+docker compose down && docker compose up -d
 
 cat << EOF
 A geração dos certificados e a inicialização do serviço pode levar
